5bca35526974229478caaf5b7f3d55f93741fd04
dev-miguel-resources <ingenieromiguelch@gmail.com>
2024-12-12 17:32:21 -0300

    first commit

---
+++ src/main/java/mx/txalcala/spring_reactor_app/SpringReactorAppApplication.java
@@ -0,0 +1,13 @@
+package mx.txalcala.spring_reactor_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/txalcala/spring_reactor_app/SpringReactorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.txalcala.spring_reactor_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
